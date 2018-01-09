6948df1bccae61a3b75fcada807cd9303102547e
zdunke1 <zdunke1@students.towson.edu>
2018-01-08 21:55:56 -0500

    Added Position Data CSV and method

--- position_classification.py
+++ position_classification.py
@@ -9,17 +9,15 @@ fifa = pd.read_csv("./position_stats.csv")
 position = pd.DataFrame(np.random.randint(low=0, high=5, size=(len(fifa), 1)),columns=['positions'])
 fifa = fifa.append(position)
 cols = fifa.columns.values
+#data = pd.DataFrame()
+#data.append()
+defd = fifa.loc[(fifa["prefers_cb"]==True)|(fifa["prefers_lb"]==True)|(fifa["prefers_rb"]==True)]
+defd["position"] = "def"
+mid = fifa.loc[(fifa["prefers_cm"]==True)|(fifa["prefers_cdm"]==True)|(fifa["prefers_cam"]==True)|(fifa["prefers_lm"]==True)|(fifa["prefers_rm"]==True)]
+mid['position'] = "mid"
+att = fifa.loc[(fifa["prefers_st"]==True)|(fifa["prefers_lw"]==True)|(fifa["prefers_rw"]==True)]
+att['position'] = "att"
 
-fifa.loc[fifa["prefers_cb"]==True, 'positions'] = "def"
-fifa.loc[fifa["prefers_lb"]==True, 'positions'] = "def"
-fifa.loc[fifa["prefers_rb"]==True, 'positions'] = "def"
-fifa.loc[fifa["prefers_cm"]==True, 'positions'] = "mid"
-fifa.loc[fifa["prefers_cam"]==True, 'positions'] = "mid"
-fifa.loc[fifa["prefers_cdm"]==True, 'positions'] = "mid"
-fifa.loc[fifa["prefers_lm"]==True, 'positions'] = "mid"
-fifa.loc[fifa["prefers_rm"]==True, 'positions'] = "mid"
-fifa.loc[fifa["prefers_st"]==True, 'positions'] = "att"
-fifa.loc[fifa["prefers_lw"]==True, 'positions'] = "att"
-fifa.loc[fifa["prefers_rw"]==True, 'positions'] = "att"
-
-print(fifa["positions"])
+data = [defd, mid, att]
+result = pd.concat(data)
+result.to_csv("./position_data.csv")
